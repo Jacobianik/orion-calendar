285dfb9f4e45b0f359ad5bc8bdf95ae9e157bd94
Yanik Ammann <confused@ant.lgbt>
2022-02-19 22:44:18 +0100

    base

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer>
+    <span>Made by <a href="//#" target="_blank" rel="noopener noreferrer">Jacob Haap</a> and <a href="//#" target="_blank" rel="noopener noreferrer">Yanik Ammann</a></span>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
